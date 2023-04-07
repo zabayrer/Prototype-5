cf2271d6783259dc5e2db060bfbaf518f65a6a32
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-04-07 12:28:27 -0700

    Finished 5.2

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class gameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
     IEnumerator SpawnTarget()
--- Assets/Scripts/target.cs
+++ Assets/Scripts/target.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
-
+    public int pointValue;
+    public ParticleSystem exposionParticle;
 
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("gameManager").GetComponent<GameManager>();
 
         targetRb.AddForce(randomForce(), ForceMode.Impulse);
         targetRb.AddTorque(randomTorque(), randomTorque(), randomTorque(),  ForceMode.Impulse);
@@ -33,6 +36,8 @@ public class target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(exposionParticle, transform.position, exposionParticle.transform.rotation);
     }
 
     private void OnTriggerEnter(Collider other)
